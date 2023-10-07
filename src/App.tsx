46466a1eecce8b8a60cf419fccdd5d7c4e583aec
Guibert Alexis <blysboutiquee@gmail.com>
2023-10-07 16:20:17 +0200

    maj rapide

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,37 @@ function App() {
   let [lng, setLng] = useState(0)
   let [accuracy, setAccuracy] = useState(0)
 
+  let [listOfArea, setListOfArea] = useState([
+    {xPos: 47.9696735, yPos: -1.8683154, size: 50},
+    {xPos: 47.9686735, yPos: -1.8683154, size: 50},
+    {xPos: 47.9684735, yPos: -1.8643154, size: 50},
+  ])
+
+  const GAME_SIZE = 200
+
+  let startPoint
+  if(!startPoint){
+    startPoint = {xPos: lat, yPos: lng, gameSize: GAME_SIZE}
+  }
+
+  const inCircle = (lat: number, lng: number, circle: { xPos: number, yPos: number, size: number }) => {
+    if(
+        lat < circle.xPos + .000008993 * circle.size &&
+        lat > (circle.xPos + .000008993 * circle.size) - 2 * circle.size * .000008993 &&
+        lng < circle.yPos + .000013464 * circle.size &&
+        lng > (circle.yPos + .000013464 * circle.size) - 2 * circle.size * .000013464
+    ){
+        const currentLat = (lat - circle.xPos) / (.000008993 * circle.size)
+        const sqrtLat = Math.pow(currentLat * 2, 2)
+
+        const currentLng = (lng - circle.yPos) / (.000013464 * circle.size)
+        const sqrtLng = Math.pow(currentLng * 2, 2)
+        
+        return sqrtLat + sqrtLng <= 4
+    }
+    return false
+  }
+
   function success(pos: GeolocationPosition) {
     var crd = pos.coords;
 
@@ -15,22 +46,40 @@ function App() {
     setLng(crd.longitude)
     setAccuracy(crd.accuracy)
 
-    if((crd.latitude > 47.9687735 && crd.latitude < 47.9687735 + .00035) && (crd.longitude > -1.8659154 && crd.longitude < -1.8659154 + .000525)){
-      alert('Zone atteinte')
-    }
+    modifyPos()
   }
 
+  
   const modifyPos = () => {
-    navigator.geolocation.getCurrentPosition(success)
+    let allCircleTests: boolean[] = []
+    listOfArea.forEach(circle => {
+      allCircleTests.push(inCircle(lat, lng, circle))
+    })
+
+    const listOfNotReachedCircle: {
+      xPos: number;
+      yPos: number;
+      size: number;
+    }[] = []
+
+    allCircleTests.forEach((isHere, index) => {
+      if(!isHere) listOfNotReachedCircle.push(listOfArea[index])
+    })
+
+    setListOfArea(listOfNotReachedCircle)
   }
 
-  setInterval(modifyPos, 1000)
+  // setTimeout(modifyPos, 1000)
+
+  navigator.geolocation.watchPosition(success)
 
   return <div className='App'>
     <Map
       lat = {lat}
       lng = {lng}
       accuracy={accuracy}
+      listOfArea = {listOfArea}
+      startPoint = {startPoint}
     />
     <div className='infoCoordinates'>
       <p>{lat}</p>
--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,39 +1,41 @@
 import './map.css'
-import { MapContainer, TileLayer, useMap, Marker, Popup, Circle, Rectangle } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet'
 
 type Props = {
     lat: number
     lng: number
     accuracy: number
+    listOfArea: {
+        xPos: number;
+        yPos: number;
+        size: number;
+    }[]
+    startPoint: {xPos: number, yPos: number, gameSize: number}
 }
 
-const Map: React.FC<Props> = ({lat, lng, accuracy}) => {
+const Map: React.FC<Props> = ({lat, lng, accuracy, listOfArea, startPoint}) => {
 
-    // const rectangleLat = 47.9687735
-    // const rectangleLatAuto = rectangleLat + .0035
-    // const rectangleLng = -1.8652154
-    // const rectangleLngAuto = rectangleLng + .00525
-
-    const rectangleLat = 47.9687735
-    const rectangleLatAuto = rectangleLat + .00035
-    const rectangleLng = -1.8659154
-    const rectangleLngAuto = rectangleLng + .000525
+    const createCircles = (listOfArea: {
+            xPos: number;
+            yPos: number;
+            size: number;
+        }[]) => {
+        return listOfArea.map((circle, i) => {
+            return <Circle key={i} center={[circle.xPos, circle.yPos]} pathOptions={{ fillColor: 'blue' }} radius={circle.size} />
+        })
+      }
 
     return <MapContainer center={[47.9686735, -1.8663154]} zoom={13} scrollWheelZoom={true}>
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[lat, lng]}>
-            <Popup>
-                Starting point
-            </Popup>
-        </Marker>
-        <Rectangle bounds={[[rectangleLat, rectangleLng], [rectangleLatAuto, rectangleLngAuto]]} pathOptions={{ color: 'blue', weight: 1 }} />
+
         <Marker position={[lat, lng]}></Marker>
-        {/* <Circle center={[parcThabor.lat + 0.009, parcThabor.lng + 0.001]} pathOptions={{ fillColor: 'blue' }} radius={100} />
-        <Circle center={[parcThabor.lat - 0.005, parcThabor.lng + 0.007]} pathOptions={{ fillColor: 'blue' }} radius={100} />
-        <Circle center={[parcThabor.lat + 0.002, parcThabor.lng - 0.004]} pathOptions={{ fillColor: 'blue' }} radius={100} /> */}
+
+        {createCircles(listOfArea)}
+        <Circle key={'startCircle'} center={[startPoint.xPos, startPoint.yPos]} pathOptions={{ fillColor: 'none', color: 'black' }} radius={startPoint.gameSize} />
+
     </MapContainer>
 }
 
