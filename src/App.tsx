2afa42e3d7e4fd5f9678e40cc423d7c3c49c0f64
Guibert Alexis <blysboutiquee@gmail.com>
2023-10-08 22:05:37 +0200

    maj rapide

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,39 @@ function App() {
 
   let [gameCircle, setgameCircle] = useState({xPos: 0, yPos: 0, size: 0})
 
-  let [listOfArea, setListOfArea] = useState([
-    {xPos: 47.9686742, yPos: -1.8663187, size: 100},
-    {xPos: 48.0903168, yPos: -1.6842752, size: 100},
-  ])
-
+  const NB_CHECKPOINT = 20
   const GAME_SIZE = 3000
+  const CHEKPOINT_SIZE = 100
+  const CHECKPOINT_TIMELAPSE_M = 15
+  const UPDATE_GAME_TIMELAPSE_MS = 1000
+  
+  const GAME_TIME_M = 60.1
+  const COORDS_FINAL_CHECKPOINT = [48.0943168, -1.7142752]
+  const SPEED_OF_WALK_KMH = 3
+
+  let [restrictedCircle, setRestrictedCircle] = useState({xPos: 0, yPos: 0, size: 0})
+
+  const MAX_MINUTES_MOVE_IN_GAME = (GAME_SIZE / (SPEED_OF_WALK_KMH * 1000)) * 60
+  const SPEED_OF_CLOSING_M_TIMELAPSE = (GAME_SIZE - CHEKPOINT_SIZE)/(MAX_MINUTES_MOVE_IN_GAME*60)
+
+  let [restMinutesGame, setRestMinutesGame] = useState(GAME_TIME_M)
+
+  if(restrictedCircle.size == 0 && restMinutesGame <= MAX_MINUTES_MOVE_IN_GAME){
+    console.log('restriction')
+    setRestrictedCircle({xPos: gameCircle.xPos, yPos: gameCircle.yPos, size: GAME_SIZE})
+  }
+
+  const createAllEmptyCheckpoints = () => {
+    const listOfChekpointNumber = []
+    for(let i = 0; i < NB_CHECKPOINT; i++){
+      listOfChekpointNumber.push(i)
+    }
+    return listOfChekpointNumber.map(number => {
+      return {xPos: 0, yPos: 0, size: 0, lifeTime: CHECKPOINT_TIMELAPSE_M * 60}
+    })
+  }
+
+  let [listOfArea, setListOfArea] = useState(createAllEmptyCheckpoints())
 
   if(gameCircle.size === 0 && lat !== 0 && lng !== 0) {
     setgameCircle({xPos: lat, yPos: lng, size: GAME_SIZE})
@@ -42,6 +69,23 @@ function App() {
   }
 
   function success(pos: GeolocationPosition) {
+
+    if(restrictedCircle.size != 0 && restrictedCircle.size > CHEKPOINT_SIZE){
+      setRestrictedCircle({
+        xPos: restrictedCircle.xPos - (gameCircle.xPos - COORDS_FINAL_CHECKPOINT[0]) / (MAX_MINUTES_MOVE_IN_GAME * 60),
+        yPos: restrictedCircle.yPos - (gameCircle.yPos - COORDS_FINAL_CHECKPOINT[1]) / (MAX_MINUTES_MOVE_IN_GAME * 60),
+        size: restrictedCircle.size - SPEED_OF_CLOSING_M_TIMELAPSE
+      })
+
+      console.log({
+        xPos: restrictedCircle.xPos - (gameCircle.xPos - COORDS_FINAL_CHECKPOINT[0]) / (MAX_MINUTES_MOVE_IN_GAME * 60),
+        yPos: restrictedCircle.yPos - (gameCircle.yPos - COORDS_FINAL_CHECKPOINT[1]) / (MAX_MINUTES_MOVE_IN_GAME * 60),
+        size: restrictedCircle.size - SPEED_OF_CLOSING_M_TIMELAPSE
+      })
+    }
+
+    setRestMinutesGame(restMinutesGame - UPDATE_GAME_TIMELAPSE_MS/(1000 * 60))
+
     var crd = pos.coords;
 
     setLat(crd.latitude)
@@ -50,6 +94,19 @@ function App() {
 
     setListOfNotReachedCircle(crd.latitude, crd.longitude)
   }
+
+  const printTimeLapse = (lifeTime: number) => {
+
+    lifeTime = lifeTime * 60
+
+    if(lifeTime >= 3600){
+        return `Temps restant: ${Math.trunc(lifeTime/3600)}h ${Math.trunc((lifeTime%3600)/60)}m ${Math.trunc((lifeTime%3600)%60)}s`
+    }
+    if(lifeTime >= 60){
+        return `Temps restant: ${Math.trunc(lifeTime/60)}m ${Math.trunc(lifeTime%60)}s`
+    }
+    return `Temps restant: ${Math.trunc(lifeTime)}s`
+  }
   
   const setListOfNotReachedCircle = (lat: number, lng: number) => {
 
@@ -57,22 +114,39 @@ function App() {
       xPos: number;
       yPos: number;
       size: number;
+      lifeTime: number;
     }[] = []
 
+    const addFreeArea = () => {
+      const upToDateGameCircle = gameCircle.xPos == 0 ? {xPos: lat, yPos: lng, size: GAME_SIZE} : gameCircle
+      const actualLimitCircle = restrictedCircle.size != 0 ? restrictedCircle : upToDateGameCircle
+      let newCircle: {xPos: number, yPos: number, size: number, lifeTime: number}
+      do newCircle = findClearAreaCoordinates(CHEKPOINT_SIZE, upToDateGameCircle)
+      while(!inCircle(newCircle.xPos, newCircle.yPos, actualLimitCircle) || inCircle(lat, lng, newCircle))
+
+      return newCircle
+    }
+
+    let winPointAdded = 0
     listOfArea.forEach((circle, index) => {
-      if(!inCircle(lat, lng, circle)) listOfNotReachedCircle.push(listOfArea[index])
-      else {
-        const upToDateGameCircle = gameCircle.xPos == 0 ? {xPos: lat, yPos: lng, size: GAME_SIZE} : gameCircle
-        let newCircle: {xPos: number, yPos: number, size: number}
-        do newCircle = findClearAreaCoordinates(100, upToDateGameCircle)
-        while(!inCircle(newCircle.xPos, newCircle.yPos, upToDateGameCircle) || inCircle(lat, lng, newCircle))
+      if(circle.size == 0) listOfNotReachedCircle.push(addFreeArea())
 
+      else if(circle.lifeTime - UPDATE_GAME_TIMELAPSE_MS/1000 <= 0){
+        listOfNotReachedCircle.push(addFreeArea())
+      }
+
+      else if(!inCircle(lat, lng, circle)) {
+        const newCircle = {xPos: listOfArea[index].xPos, yPos: listOfArea[index].yPos, size: listOfArea[index].size, lifeTime: listOfArea[index].lifeTime - UPDATE_GAME_TIMELAPSE_MS/1000}
         listOfNotReachedCircle.push(newCircle)
+      }
 
-        setWinPoint(winPoint + 1)
+      else {
+        listOfNotReachedCircle.push(addFreeArea())
+        winPointAdded += 1
       }
     })
 
+    setWinPoint(winPointAdded)
     setListOfArea(listOfNotReachedCircle)
   }
 
@@ -89,12 +163,20 @@ function App() {
     const newXPos = upToDateGameCircle.xPos + xRadius
     const newYPos = upToDateGameCircle.yPos + yRadius
 
-    return {xPos: newXPos, yPos: newYPos, size}
+    return {xPos: newXPos, yPos: newYPos, size, lifeTime: CHECKPOINT_TIMELAPSE_M * 60}
   }
 
   const getPosition = () => navigator.geolocation.getCurrentPosition(success)
-  setTimeout(getPosition, 1000)
+  setTimeout(getPosition, UPDATE_GAME_TIMELAPSE_MS)
 
+  if(restMinutesGame == 0){
+    alert('Jeu terminé')
+  }
+
+  if(gameCircle.size == 0){
+    return <div className='App'>
+    </div>
+  }
   return <div className='App'>
     <Map
       lat = {lat}
@@ -102,12 +184,14 @@ function App() {
       accuracy={accuracy}
       listOfArea = {listOfArea}
       gameCircle = {gameCircle}
+      restrictedCircle = {restrictedCircle}
+      COORDS_FINAL_CHECKPOINT = {COORDS_FINAL_CHECKPOINT}
+      CHECKPOINT_TIMELAPSE_M = {CHECKPOINT_TIMELAPSE_M}
     />
     <div className='infoCoordinates'>
-      <p>xPos: {lat}</p>
-      <p>yPos: {lng}</p>
       <p>Accuracy: {Math.floor(accuracy)}m</p>
       <p>Winpoint: {winPoint}</p>
+      <p>{printTimeLapse(restMinutesGame)}</p>
     </div>
   </div>
 }
--- src/map/map.tsx
+++ src/map/map.tsx
@@ -1,5 +1,5 @@
 import './map.css'
-import { MapContainer, TileLayer, Marker, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Circle, Popup } from 'react-leaflet'
 
 type Props = {
     lat: number
@@ -9,35 +9,71 @@ type Props = {
         xPos: number;
         yPos: number;
         size: number;
+        lifeTime: number;
     }[]
     gameCircle: {xPos: number, yPos: number, size: number}
+    restrictedCircle: {xPos: number, yPos: number, size: number}
+    CHECKPOINT_TIMELAPSE_M: number
+    COORDS_FINAL_CHECKPOINT: number[]
 }
 
-const Map: React.FC<Props> = ({lat, lng, accuracy, listOfArea, gameCircle}) => {
+const Map: React.FC<Props> = ({lat, lng, accuracy, listOfArea, gameCircle, restrictedCircle, CHECKPOINT_TIMELAPSE_M, COORDS_FINAL_CHECKPOINT}) => {
 
-    const circlesColor = 'red'
+    const circlesColor = 'green'
 
     const createCircles = (listOfArea: {
             xPos: number;
             yPos: number;
             size: number;
+            lifeTime: number;
         }[]) => {
         return listOfArea.map((circle, i) => {
+
+            const printTimeLapse = (lifeTime: number) => {
+
+                if(lifeTime >= 3600){
+                    return `Temps restant: ${Math.trunc(lifeTime/3600)}h ${Math.trunc((lifeTime%3600)/60)}m ${(lifeTime%3600)%60}s`
+                }
+                if(lifeTime >= 60){
+                    return `Temps restant: ${Math.trunc(lifeTime/60)}m ${lifeTime%60}s`
+                }
+                return `Temps restant: ${lifeTime}s`
+            }
+
+            const colorByTimeLapse = (lifeTime: number, CHECKPOINT_TIMELAPSE_M: number) => {
+
+                const totalSeconds = CHECKPOINT_TIMELAPSE_M * 60
+                const secondsOfRest = lifeTime
+
+                const ratio = secondsOfRest/totalSeconds
+
+                const red = 255 - (255 * ratio)
+                const green = 255 * ratio
+
+                return `rgb(${red}, 0, ${green})`
+            }
+
             if(circle.size > 0){
-                return <Circle key={i} center={[circle.xPos, circle.yPos]} pathOptions={{ fillColor: circlesColor, color: circlesColor }} radius={circle.size} />
+                return <Circle key={i} center={[circle.xPos, circle.yPos]} pathOptions={{ fillColor: colorByTimeLapse(circle.lifeTime, CHECKPOINT_TIMELAPSE_M), color: colorByTimeLapse(circle.lifeTime, CHECKPOINT_TIMELAPSE_M) }} radius={circle.size}>
+                    <Popup>
+                        {printTimeLapse(circle.lifeTime)}
+                    </Popup>
+                </Circle>
             }
         })
       }
 
-    return <MapContainer center={[47.9686735, -1.8663154]} zoom={11} scrollWheelZoom={true}>
+    return <MapContainer center={[lat, lng]} zoom={14} scrollWheelZoom={true}>
         <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
         <Marker position={[lat, lng]}></Marker>
+        <Marker position={[COORDS_FINAL_CHECKPOINT[0], COORDS_FINAL_CHECKPOINT[1]]}></Marker>
 
         {createCircles(listOfArea)}
+        <Circle key={'restrictedCircle'} center={[restrictedCircle.xPos, restrictedCircle.yPos]} pathOptions={{ fillColor: 'none', color: 'red' }} radius={restrictedCircle.size} />
         <Circle key={'gameCircle'} center={[gameCircle.xPos, gameCircle.yPos]} pathOptions={{ fillColor: 'none', color: 'black' }} radius={gameCircle.size} />
 
     </MapContainer>
